41b97c2cf834a104016cb979be2db22092726e4e
undrafted <8jermiedomingo@gmail.com>
2020-04-16 20:21:48 +0200

    move cursor position state out of context

--- src/lib/CursorProvider.tsx
+++ src/lib/CursorProvider.tsx
@@ -4,14 +4,17 @@ import { CursorProviderWrapper } from './CursorContext'
 
 const CursorProvider: React.FC<any> = ({ config, children }) => {
   const [currentCursor, setCurrentCursor] = useState('default-cursor')
-  const [contextValue] = useState({
+
+  const contextValue = {
+    ...config,
     cursors: [
       { id: 'default-cursor', component: () => <div>☄️</div> },
       ...config.cursors,
     ],
     setCurrentCursor,
-  })
+  }
 
+  // The rest of your rendering logic
   return (
     <CursorProviderWrapper value={contextValue}>
       <CurrentCursor nextCursor={currentCursor} />
