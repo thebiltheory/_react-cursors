8095b029708669a3ef1b2367a9dfb1f07f2ad7ee
undrafted <8jermiedomingo@gmail.com>
2020-04-16 19:31:54 +0200

    refactor useHover, listen to mouseleave

--- src/utils/useHover.ts
+++ src/utils/useHover.ts
@@ -1,45 +1,29 @@
-import { useState, useRef, useCallback } from 'react'
+import { useState, useRef, useCallback, useEffect } from 'react'
 
 export default function useHover() {
   const [value, setValue] = useState(false)
 
   const ref = useRef<any>(null)
-  const timerRef = useRef<any>(null)
 
   const handleMouseOver = useCallback(() => {
-    if (timerRef.current) {
-      clearTimeout(timerRef.current)
-    }
-    timerRef.current = setTimeout(() => {
-      setValue(true)
-    }, 0)
+    setValue(true)
   }, [setValue])
 
   const handleMouseOut = useCallback(() => {
-    if (timerRef.current) {
-      clearTimeout(timerRef.current)
-    }
-    timerRef.current = setTimeout(() => {
-      setValue(false)
-    }, 0)
+    setValue(false)
   }, [setValue])
 
-  const callbackRef = useCallback(
-    (node: any): any => {
-      if (ref.current) {
-        ref.current.removeEventListener('mouseover', handleMouseOver)
-        ref.current.removeEventListener('mouseout', handleMouseOut)
-      }
+  useEffect(() => {
+    if (ref && ref.current) {
+      ref.current.addEventListener('mouseover', handleMouseOver)
+      ref.current.addEventListener('mouseleave', handleMouseOut)
+    }
 
-      ref.current = node
+    return () => {
+      ref.current.removeEventListener('mouseover', handleMouseOver)
+      ref.current.removeEventListener('mouseleave', handleMouseOut)
+    }
+  }, [ref, handleMouseOver, handleMouseOut])
 
-      if (ref.current) {
-        ref.current.addEventListener('mouseover', handleMouseOver)
-        ref.current.addEventListener('mouseout', handleMouseOut)
-      }
-    },
-    [handleMouseOut, handleMouseOver]
-  )
-
-  return [callbackRef, value]
+  return [ref, value]
 }
